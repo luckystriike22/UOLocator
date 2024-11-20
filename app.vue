a788a40c0815be4aac0db187026ee76afa333cfa
Luckystriike <glenny@me.com>
2024-11-20 11:50:59 +0100

    add

---
+++ app.vue
@@ -0,0 +1,47 @@
+<template>
+  <NuxtLayout>
+    <v-app class="d-flex flex-column" style="min-height: 100vh;">
+      <header class="header-color">
+        <h1>Custom Outlands locator</h1>
+      </header>
+
+      <main class="flex-grow-1">
+        <NuxtPage />
+      </main>
+
+      <footer>
+        <p>© 2024 by Luckystriike</p>
+      </footer>
+    </v-app>
+  </NuxtLayout>
+</template>
+
+<script setup>
+</script>
+
+<style>
+header {
+  background: #f5f5f5;
+  padding: 1em;
+  text-align: center;
+}
+
+footer {
+  padding: 1em;
+  text-align: center;
+}
+
+.header-color {
+  background-color: #587184;
+}
+
+v-app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+main {
+  flex-grow: 1; /* This makes sure the main content takes up the remaining space */
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,42 @@
+<template>
+    <v-row justify="center" class="mt-10">
+        <v-col ols="12" md="6">
+        <v-text-field v-model="name" label="Locator name:"></v-text-field>
+        <v-text-field v-model="coordX" label="Coord x:" @keydown="filterInput"></v-text-field>
+        <v-text-field v-model="CoordY" label="Coord y:"></v-text-field>
+        <v-btn @click="copy" width="100%" color="#558B2F">Copy</v-btn>
+        </v-col>
+    </v-row>
+</template>
+
+<script setup>
+const name = ref("wrangler")
+const coordX = ref(1586)
+const CoordY = ref(1475)
+
+
+const filterInput = (event) => {
+  const allowedKeys = [
+    ...'0123456789.', // Allow digits and dot
+    'Backspace', // Allow backspace
+    'ArrowLeft', 'ArrowRight', 'Delete', // Allow navigation keys
+  ]
+
+  // Prevent default action if the key is not allowed
+  if (!allowedKeys.includes(event.key)) {
+    event.preventDefault()
+  }
+}
+const copy = () =>{
+    console.log(name.value)
+    var locator = `#uooutlands␟vendorlocation␟${name.value}␟${name.value}␟${coordX.value}␟${CoordY.value}␟615431␟1117799979␟1692765527`
+    var encoded = btoa(unescape(encodeURIComponent(locator)))
+    navigator.clipboard.writeText(encoded);
+}
+</script>
+
+<style>
+h1 {
+    color: #333;
+}
+</style>
